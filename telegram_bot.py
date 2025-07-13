1ed658fe1a4e6dcab904ddaeb6a7549e8eecbe59
Mapesa-code <prosperterrance@gmail.com>
2025-07-13 05:31:27 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -216,8 +216,9 @@ features = [
 X = games[features].fillna(0)
 y = games['HomeWin']
 
-from sklearn.model_selection
+
 import train_test_split
+from sklearn.model_selection
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
