3a07ddc9026bf6b1c58370aede4bdd881b581eaf
Mapesa-code <prosperterrance@gmail.com>
2025-07-13 15:24:32 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -77,8 +77,8 @@ features = [
 X = df[features]
 y = df['HomeWin']
 
-import train_test_split
-from sklearn.model_selection
+
+
 # --- Train/test split ---
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
