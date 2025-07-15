3d37748303e4786e8feeed9e8c4408d25ba070de
Mapesa-code <prosperterrance@gmail.com>
2025-07-15 20:08:10 +0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,4 @@
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 SPORTS_API_KEY = os.getenv("861c2d372b8a409b9db663cb0bc08dbc")
 OPENAI_API_KEY = os.getenv("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")
